0766cd6cb136dff7935cf96d489b4e1ffc656c53
KGrim23 <kalaya.grimshaw@gmail.com>
2024-09-28 14:32:33 +0100

    nextconfig for github pages deleted

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-// export default nextConfig;
-
-// next.config.js
-module.exports = {
-  output: "export",
-  distDir: "out",
-};
+export default nextConfig;
