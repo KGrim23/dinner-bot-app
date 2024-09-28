0d65bbed83e9b6969f3a4f856ae83d97fe50c545
KGrim23 <kalaya.grimshaw@gmail.com>
2024-09-28 19:11:09 +0100

    chnages to api

--- app/page.js
+++ app/page.js
@@ -65,7 +65,6 @@ export default function Home() {
       <h3 className="text-md text-gray-500 mb-4">
         Let us know what you&apos;ve got in your fridge!
       </h3>
-
       <form onSubmit={handleSubmit} className="w-full max-w-md">
         <input
           type="text"
@@ -82,56 +81,43 @@ export default function Home() {
           {loading ? "Fetching recipes..." : "Find Recipes"}
         </button>
       </form>
-
-      {/* Display the recipes */}
       <div className="mt-6 w-full max-w-md mb-6">
-        <div className="space-y-4">
-          <h2 className="text-2xl font-bold mb-4">Recipe Suggestions:</h2>
-
-          {/* Display each recipe without mapping TODO: map over the recipes*/}
-          <div className="p-4 border border-gray-300 bg-gray-300 rounded shadow-md">
-            <h3 className="text-lg font-semibold text-blue-600">Pork Chops</h3>
-            <p className="mt-2 text-gray-700">
-              Oven-roasted, pan-fried, sizzled or seared, compiled variety of
-              ways to serve deliciously tender pork chops, from posh plates to
-              everyday feasts
-            </p>
+        {error && <p className="text-red-500">{error}</p>}
+        {extractedRecipes.length > 0 && (
+          <div className="space-y-4">
+            <h2 className="text-2xl font-bold mb-4">Recipe Suggestions:</h2>
+            {/* Display each recipe without mapping TODO: map over the recipes*/}
+            <div className="p-4 border border-gray-300 bg-gray-300 rounded shadow-md">
+              <h3 className="text-lg font-semibold text-blue-600">
+                {extractedRecipes[0]?.title}
+              </h3>
+              <p className="mt-2 text-gray-700">{extractedRecipes[0]?.body}</p>
+            </div>
+            <div className="p-4 border border-gray-300 bg-gray-300  rounded shadow-md">
+              <h3 className="text-lg font-semibold text-blue-600">
+                {extractedRecipes[1]?.title}
+              </h3>
+              <p className="mt-2 text-gray-700">{extractedRecipes[1]?.body}</p>
+            </div>
+            <div className="p-4 border border-gray-300 bg-gray-300  rounded shadow-md">
+              <h3 className="text-lg font-semibold text-blue-600">
+                {extractedRecipes[2]?.title}
+              </h3>
+              <p className="mt-2 text-gray-700">{extractedRecipes[2]?.body}</p>
+            </div>
+            <div className="p-4 border border-gray-300 bg-gray-300  rounded shadow-md">
+              <h3 className="text-lg font-semibold text-blue-600">
+                {extractedRecipes[3]?.title}
+              </h3>
+              <p className="mt-2 text-gray-700">{extractedRecipes[3]?.body}</p>
+            </div>
+            <div className="p-4 mt-4 itemms-center text-center">
+              <p className=" text-pink-600 font-semibold mt-4 italic">
+                bon appétit
+              </p>
+            </div>
           </div>
-          <div className="p-4 border border-gray-300 bg-gray-300  rounded shadow-md">
-            <h3 className="text-lg font-semibold text-blue-600">
-              Filo pastry recipes
-            </h3>
-            <p className="mt-2 text-gray-700">
-              Get your fill of filo with our sweet and savoury recipes, all
-              showcasing the versatility and crisp beauty of this unique pastry.
-            </p>
-          </div>
-          <div className="p-4 border border-gray-300 bg-gray-300  rounded shadow-md">
-            <h3 className="text-lg font-semibold text-blue-600">
-              Pumpkin soup recipes
-            </h3>
-            <p className="mt-2 text-gray-700">
-              Celebrate autumn produce with our warming pumpkin soups. Perfect
-              as a starter or light lunch, these squash recipes include
-              vegetarian and healthy options.
-            </p>
-          </div>
-          <div className="p-4 border border-gray-300 bg-gray-300  rounded shadow-md">
-            <h3 className="text-lg font-semibold text-blue-600">
-              Indian recipes
-            </h3>
-            <p className="mt-2 text-gray-700">
-              Everyone loves a curry. From a big-flavoured balti to a fragrant
-              veggie biryani, and a refreshing lassi drink to refresh the
-              palate.
-            </p>
-          </div>
-          <div className="p-4 mt-4 itemms-center text-center">
-            <p className=" text-pink-600 font-semibold mt-4 italic">
-              bon appétit
-            </p>
-          </div>
-        </div>
+        )}
       </div>
     </div>
   );
