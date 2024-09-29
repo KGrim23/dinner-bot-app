2dbab3da437df078be443f92ee005e313341b67a
KGrim23 <kalaya.grimshaw@gmail.com>
2024-09-29 12:33:27 +0100

    fixed build error and chnaged api to get 3 recipes at a time

--- app/api/recipes/route.js
+++ app/api/recipes/route.js
@@ -16,7 +16,7 @@ export async function POST(req) {
 
   const prompt = `I have the following ingredients: ${ingredients.join(
     ", "
-  )}. Can you suggest 1 recipe? Each recipe should include:
+  )}. Can you suggest 3 recipe? Each recipe should include:
   1. Brief description of recipe: [Short paragraph no longer than 100 words]
   2. Title: [Recipe title]
   3. Ingredients: [List each ingredient on a new line without any symbols or - or number]
